c6ecd9eddb3fe3bcde40d8b09bbb10051dc8a68b
Juan_Carlos_Garrido <juanc.garrido.mejia@juntadeandalucia.es>
2026-02-24 11:59:34 +0100

    LINTING

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -12,7 +12,7 @@ from .populate import initiate
 
 from .models import CarMake, CarModel
 
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -104,7 +104,7 @@ def get_cars(request):
 # ...
 # Update the `get_dealerships` render list of dealerships all by
 # default, particular state if state is passed
-def get_dealerships(request, state = "All"):
+def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
     else:
@@ -118,7 +118,7 @@ def get_dealerships(request, state = "All"):
 # ...
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -147,9 +147,9 @@ def get_dealer_details(request, dealer_id):
 # ...
 def add_review(request):
     if not (request.user.is_anonymous):
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
-            #response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
