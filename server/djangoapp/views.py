7df2c7e269e11619e1ffcb1c0b0cec510acccb28
jcgarridofps <jcgarrido.fps@outlook.com>
2026-08-25 09:42:36 -0400

    proxy

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -12,7 +12,7 @@ from .populate import initiate
 
 from .models import CarMake, CarModel
 
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -149,9 +149,9 @@ def get_dealer_details(request, dealer_id):
 # ...
 def add_review(request):
     if not (request.user.is_anonymous):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
